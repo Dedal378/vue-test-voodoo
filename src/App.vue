930f8cb9d54d09ac414dd2dbf1510d79ff889d51
Dedal378 <orlovetsfoto@gmail.com>
2022-03-17 19:44:27 +0300

    final

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,93 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, onBeforeMount, ref } from 'vue'
+import axios from 'axios'
+import BaseSearch from './components/BaseSearch.vue'
+import BaseCard from './components/BaseCard.vue'
+
+const link = `https://jsonplaceholder.typicode.com`
+const postsLimit = 30
+let posts = ref([])
+let users = ref([])
+const userName = ref('')
+
+const filteredUserByName = computed(() => {
+  posts.value.filter(i1 =>
+    users.value.map(i2 => {
+      if (i2.id === i1.userId) {
+        i1.author = i2.name
+      }
+    })
+  )
+
+  return posts.value.filter(user => {
+    if (userName.value) {
+      return user.author.toLowerCase().includes(userName.value.toLowerCase())
+    }
+    return posts.value
+  })
+})
+const totalPostsNumber = computed(() => {
+  return filteredUserByName.value.length
+})
+
+const getData = async(apiName = 'posts', limit = postsLimit) => {
+  let { data } = await axios.get(`${ link }/${ apiName }?_limit=${ limit }`)
+
+  if (apiName === 'posts') {
+    posts.value = data
+  } else if (apiName === 'users') {
+    users.value = data
+  }
+}
+
+onBeforeMount(() => {
+  getData()
+  getData('users', 3)
+})
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+  <b-container class="mt-5 mb-5">
+    <b-row class="mb-5">
+      <b-col class="d-flex justify-content-center">
+        <BaseSearch v-model="userName" search />
+      </b-col>
+    </b-row>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <b-row class="mb-2 justify-content-center text-uppercase fw-light">
+      Total number of posts is
+      <strong class="px-2 text-uppercase w-auto fw-bold">{{ posts.length }}</strong> by
+      <strong class="px-2 text-uppercase w-auto fw-bold">{{ users.length }}</strong>
+      authors.
+    </b-row>
+    <b-row
+      v-if="userName && filteredUserByName.length > 0"
+      class="px-1 justify-content-center text-center fw-light"
+    >
+      <small>
+        Total number of posts
+        <i class="px-2 text-uppercase w-auto">{{ userName }}</i>
+        is<i class="px-2 text-uppercase w-auto">{{ totalPostsNumber }}</i>.
+      </small>
+    </b-row>
+    <b-row
+      v-if="filteredUserByName.length === 0"
+      class="px-1 justify-content-center text-center fst-italic"
+    >
+      Nothing found. There are no posts to show.
+    </b-row>
+
+    <b-row
+      class="mb-3 mt-5 d-flex flex-wrap container-fluid justify-content-center align-items-start"
+    >
+      <BaseCard
+        v-for="(post, idx) in filteredUserByName"
+        :key="idx"
+        :author="post.author"
+        :post="post.body"
+        :title="post.title"
+        class="m-2"
+      />
+    </b-row>
+  </b-container>
+</template>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { BCardText } from 'bootstrap-vue-3'
+
+defineProps({
+  post: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <b-card
+    :header="title"
+    header-bg-variant="white"
+    header-text-variant="primary"
+    style="max-width: 18rem"
+    class="d-flex align-items-around justify-content-between"
+  >
+    <b-card-text>{{ post }}</b-card-text>
+    <b-card-text class="text-secondary fw-lighter">{{ author }}</b-card-text>
+  </b-card>
+</template>
